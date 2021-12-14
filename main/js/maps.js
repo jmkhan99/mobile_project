9a4aab3e35aca82cca4d36668a9a39cb16c66102
JM <jmkhana@gmail.com>
2021-12-14 19:51:51 +0900

    20211214

---
+++ main/js/maps.js
@@ -0,0 +1,24 @@
+﻿var mapOptions = {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10
+};
+
+var position = navigator.geolocation.getCurrentPosition(on_geo, error_geo) 
+
+function on_geo(){
+	console.log(position);
+}
+function error_geo(){
+	alert('a');
+}
+
+var map = new naver.maps.Map('map', mapOptions);
+
+var map = null;
+
+function initMap() {
+    map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng(37.3595704, 127.105399),
+        zoom: 10
+    });
+}
