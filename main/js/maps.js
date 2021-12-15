581cf0f6766976f891270916dc487cf11dac4949
unknown <jmkhana@gmail.com>
2021-12-15 09:20:39 +0900

    20211215

--- main/js/maps.js
+++ main/js/maps.js
@@ -1,21 +1,31 @@
 ﻿navigator.geolocation.getCurrentPosition(on_geo, error_geo) 
 
-function on_geo(position){
-	var lat = position.coords.latitude
-	var lng = position.coords.longitude
-	var mapOptions = {
-		center: new naver.maps.LatLng(lat, lng),
-		zoom: 19
-	};
-	var map = new naver.maps.Map('map', mapOptions);
-	
-	var markerOptions = {
-		position: new naver.maps.LatLng(lat, lng),
-		map: map
-	};
-	var marker = new naver.maps.Marker(markerOptions)
+var container = document.getElementById('map'),
+    options = {
+         center: new kakao.maps.LatLng(33.450701, 126.570667),
+         level: 3
+    };
+ 
+var map = new kakao.maps.Map(container, options);
 
-}
-function error_geo(){
-	return;
-}
+
+// 네이버
+// function on_geo(position){
+	// var lat = position.coords.latitude
+	// var lng = position.coords.longitude
+	// var mapOptions = {
+		// center: new naver.maps.LatLng(lat, lng),
+		// zoom: 19
+	// };
+	// var map = new naver.maps.Map('map', mapOptions);
+	
+	// var markerOptions = {
+		// position: new naver.maps.LatLng(lat, lng),
+		// map: map
+	// };
+	// var marker = new naver.maps.Marker(markerOptions)
+
+// }
+// function error_geo(){
+	// return;
+// }
